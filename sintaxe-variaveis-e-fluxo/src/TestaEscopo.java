de5955f6210aeecc121c8e47addbc07608ebb286
wesleyfelix <wesley.felixdelima96@gmail.com>
2021-11-10 14:48:37 -0300

    while

---
+++ sintaxe-variaveis-e-fluxo/src/TestaEscopo.java
@@ -0,0 +1,29 @@
+
+public class TestaEscopo {
+
+	public static void main(String[] args) {
+
+		System.out.println("testando condicionais\n");
+		int idade = 20;
+		int quantidadePessoas = 3;
+//		boolean acompanhado = quantidadePessoas >= 2;
+
+		boolean acompanhado;
+
+		if (quantidadePessoas >= 2) {
+			acompanhado = true;
+		} else {
+			acompanhado = false;
+		}
+
+		System.out.println("valor acompanhado"+acompanhado);
+
+		if (idade >= 18 && acompanhado) {
+			System.out.println("seja bem vindo");
+		} else {
+			System.out.println("infelizmente você não pode entrar");
+		}
+
+	}
+
+}
