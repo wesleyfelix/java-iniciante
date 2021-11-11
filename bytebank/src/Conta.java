2ab605072eab2b482e13fbf6c331752bf3efdd16
wesleyfelix <wesley.felixdelima96@gmail.com>
2021-11-11 13:27:46 -0300

    metodos com mais de um objeto

--- bytebank/src/Conta.java
+++ bytebank/src/Conta.java
@@ -13,9 +13,17 @@ class Conta {
 			this.saldo -= valor;
 			return true;
 		}
-		else {
-			return false;
-		}
+		return false;
 	}
+	
+	public boolean transfere(double valor, Conta destino){
+		if(this.saldo >= valor) {
+			this.saca(valor);
+			destino.deposita(valor);;
+			return true;
+		}
+		return false;
+	}
+		
 }
 	
--- bytebank/src/TestaMetodo.java
+++ bytebank/src/TestaMetodo.java
@@ -2,18 +2,31 @@
 public class TestaMetodo {
 
 	public static void main(String[] args) {
-		Conta conta = new Conta();
-		conta.saldo = 100;
-		conta.deposita(50);
-		System.out.println(conta.saldo);
+		Conta contaDoWesley = new Conta();
+		contaDoWesley.saldo = 100;
+		contaDoWesley.deposita(50);
+		System.out.println(contaDoWesley.saldo);
 		
-		double saque = 200;
-		boolean conseguiuRetirar = conta.saca(saque);
-		System.out.println(conta.saldo);
+		double saque = 20;
+		boolean conseguiuRetirar = contaDoWesley.saca(saque);
+		System.out.println(contaDoWesley.saldo);
+		
 		System.out.println(conseguiuRetirar? 
 				"Saque aprovado de: R$"
 				+ saque 
 				+" - saldo atual: "
-				+ conta.saldo: "Sem dinheiro para sacar");
+				+ contaDoWesley.saldo: "Sem dinheiro para sacar");
+	
+		Conta contaDaDaniela = new Conta();
+		contaDaDaniela.deposita(1000);
+		
+		boolean sucessoNaTransferencia = contaDaDaniela.transfere(300, contaDoWesley);
+		
+		System.out.println(contaDoWesley.saldo);
+		System.out.println(contaDaDaniela.saldo);
+		
+		System.out.println(sucessoNaTransferencia?"Deu certo a transferencia": "Faltou dinheiro");
+		
+		
 	}
 }
