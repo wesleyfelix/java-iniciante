4b760ccd9d71f05230e0396d453dd325f40ed615
wesleyfelix <wesley.felixdelima96@gmail.com>
2021-11-11 21:28:45 -0300

    get e set

--- bytebank-composto/src/TestaContaSemCliente.java
+++ bytebank-composto/src/TestaContaSemCliente.java
@@ -4,7 +4,7 @@ public class TestaContaSemCliente {
 	public static void main(String[] args) {
 		Conta ContaDaDaniela = new Conta();
 		
-		System.out.println(ContaDaDaniela.saldo);
+		System.out.println(ContaDaDaniela.getSaldo());
 		
 		ContaDaDaniela.titular = new Cliente();
 		
--- bytebank-encapsulado/src/Conta.java
+++ bytebank-encapsulado/src/Conta.java
@@ -1,8 +1,8 @@
 class Conta {
 	private double saldo;
-	int agencia;
-	int numero;
-	Cliente titular;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
 	
 	void deposita(double valor) {
 		this.saldo += valor;
@@ -29,5 +29,22 @@ class Conta {
 	public double getSaldo() {
 		return this.saldo;
 	}
+	
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public int getAgencia() {
+		return this.agencia;
+	}
+	
+	public void setAgencia(int numero) {
+		this.agencia = numero;
+	}
+	
 }
 	
